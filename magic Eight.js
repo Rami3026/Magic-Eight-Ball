603ded0e5124be727307a2c63e4f7ef3d2ad904c
Rami3026 <91858420+Rami3026@users.noreply.github.com>
2021-11-06 11:39:48 +0000

    Add files via upload

---
+++ magic Eight.js
@@ -0,0 +1,5 @@
+let userName = '';
+Michael ? console.log("Hello!") : console.log ("Hello!");
+const userQuestion = 'Will I become a werewolf tonight?';
+console.log(`The user asked: ${Magic Eight Ball}`);
+Math.floor(Math.random() * 8);
